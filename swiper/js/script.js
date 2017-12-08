519d1f837f7c7594b08754226bbc5243bdc99277
1005626739 <1005626739@qq.com>
2017-12-08 10:49:58 +0800

    1

--- swiper/js/script.js
+++ swiper/js/script.js
@@ -1,14 +1,20 @@
 var swiper = new Swiper('.swiper-container', {
-    //设置滑动方向
     direction: 'vertical',
-    // 向右的按钮
     navigation: {
         nextEl: '.swiper-button-next',
     },
-    //设置分页小按钮
+
     pagination: {
         el: '.swiper-pagination',
-        //设置小按钮是否可以点击
         clickable: true,
     },
+    on:{
+        init: function(){
+            swiperAnimateCache(this); //隐藏动画元素
+            swiperAnimate(this); //初始化完成开始动画
+        },
+        slideChangeTransitionEnd: function(){
+            swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
+        }
+    }
 });
