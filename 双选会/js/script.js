0f913365efccda493140f7fb0debe1d09f9149b9
1005626739 <1005626739@qq.com>
2017-12-14 14:45:11 +0800

    1

---
+++ 双选会/js/script.js
@@ -0,0 +1,36 @@
+var swiper = new Swiper('.swiper-container', {
+    direction: 'vertical',
+    navigation: {
+        nextEl: '.swiper-button-next',
+    },
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    on:{
+        init: function(){
+            swiperAnimateCache(this);
+            swiperAnimate(this);
+        },
+        slideChangeTransitionEnd: function(){
+            swiperAnimate(this);
+        }
+    }
+});
+var mp3 = document.querySelector("#mp3");
+
+var mp3Btn = document.querySelector("#mp3Btn");
+//mp3Btn.style.animationPlayState = 'paused'
+
+//mp3Btn.style.animationPlayState = 'running'
+
+mp3Btn.onclick = function () {
+    if (mp3.paused) {
+        mp3Btn.style.animationPlayState = 'running';
+        mp3.play();
+    } else {
+        mp3Btn.style.animationPlayState = 'paused';
+        mp3.pause();
+    }
+}
